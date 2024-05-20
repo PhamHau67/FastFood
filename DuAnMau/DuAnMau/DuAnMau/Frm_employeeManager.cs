e5ea3b6c8754ecaa68d4ba717a3f7b04beb2f06a
HungYeager <123phuhung.dl@gmail.com>
2024-05-20 12:13:30 +0700

    update form employee manager

--- DuAnMau/DuAnMau/DuAnMau/Frm_employeeManager.cs
+++ DuAnMau/DuAnMau/DuAnMau/Frm_employeeManager.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Linq;
 
 namespace DuAnMau
 {
     public partial class Frm_employeeManager : Form
     {
+        private string _con = "Data Source=DESKTOP-R9SVLJT\\HUNG;Initial Catalog=FastFoodDB;Integrated Security=True";
         public Frm_employeeManager()
         {
             InitializeComponent();
+            Load_dgv_manager();
+        }
+        public void Load_dgv_manager()
+        {
+            using (var db = new DataClasses1DataContext(_con))
+            {
+                var query = from nv in db.NHAN_VIENs
+                                   select new
+                                   {
+                                       nv.MaNhanVien,
+                                       nv.TenNhanVien,
+                                       nv.CCCD,
+                                       nv.MaBoPhan,
+                                       nv.MaVaiTro,
+                                       nv.NgaySinh,
+                                       nv.GioiTinh,
+                                       nv.SDT,
+                                       nv.NgayDangKi,
+                                       nv.Gmail,
+                                       nv.TrangThai
+                                   };
+
+                dgv_staff.DataSource = query.ToList();
+            }
         }
     }
 }

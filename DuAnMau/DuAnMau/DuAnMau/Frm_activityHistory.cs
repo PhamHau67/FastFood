7e8f605345d92a15bd8d6414ed4e5ce0eccdf2b7
HungYeager <123phuhung.dl@gmail.com>
2024-06-14 18:23:17 +0700

    done

--- DuAnMau/DuAnMau/DuAnMau/Frm_activityHistory.cs
+++ DuAnMau/DuAnMau/DuAnMau/Frm_activityHistory.cs
@@ -23,15 +23,8 @@ namespace DuAnMau
         {
             InitializeComponent();
             Load_dgv_activity();
-            
             InitializeComboBoxes();
-            Load_dgv_activity();          
             dgv_LichSu.RowHeadersVisible = false;
-
-
-
-
-
             dtp_start.Format = DateTimePickerFormat.Custom;
             dtp_start.CustomFormat = "dd/MM/yyyy";
             dtp_end.Format = DateTimePickerFormat.Custom;
@@ -306,73 +299,8 @@ namespace DuAnMau
 
         
 
-        private void FilterData()
-        {
-            using (var db = new DataClasses1DataContext(clConn.conn))
-            {
-                var keyword = txt_find.Text.Trim();
-                var shiftFilter = cbo_shift.SelectedItem?.ToString();
-                var counterFilter = cbo_counter.SelectedItem?.ToString();
-                var statusFilter = cbo_status.SelectedItem?.ToString();
+        
 
-                bool isDateFilterUsed = dtp_start.Value.Date != DateTime.Now.Date || dtp_end.Value.Date != DateTime.Now.Date;
-
-                var startDateFilter = dtp_start.Value.Date;
-                var endDateFilter = dtp_end.Value.Date.AddDays(1).AddSeconds(-1);
-
-                var findnv = from nv in db.NHAN_VIENs
-                             join nvc in db.NHANVIEN_CAKIPs on nv.MaNhanVien equals nvc.MaNhanVien
-                             join ck in db.CAKIPs on nvc.MaCaKip equals ck.MaCaKip
-                             where (string.IsNullOrEmpty(keyword) || nv.MaNhanVien.Contains(keyword) || ck.MaCaKip.Contains(keyword) || nv.TenNhanVien.Contains(keyword) || nvc.Quay.Contains(keyword) || nvc.NgayLam.ToString().Contains(keyword))
-                                && (string.IsNullOrEmpty(shiftFilter) || nvc.MaCaKip == shiftFilter)
-                                && (string.IsNullOrEmpty(counterFilter) || nvc.Quay == counterFilter)
-                                && (string.IsNullOrEmpty(statusFilter) || (statusFilter == "Present" && nvc.TrangThai == true) || (statusFilter == "Absent" && nvc.TrangThai == false))
-                                && (!isDateFilterUsed || (nvc.NgayLam >= startDateFilter && nvc.NgayLam <= endDateFilter)) // Áp dụng bộ lọc ngày tháng nếu cần
-                             select new
-                             {
-                                 ck.MaCaKip,
-                                 ck.GioBatDau,
-                                 ck.GioKetThuc,
-                                 nv.MaNhanVien,
-                                 nv.TenNhanVien,
-                                 nvc.Quay,
-                                 nvc.NgayLam,
-                                 TrangThai = (bool)nvc.TrangThai ? "Present" : "Absent",
-                             };
-
-                DataTable dt = new DataTable();
-                dt.Columns.Add("ShiftCode");
-                dt.Columns.Add("StartTime");
-                dt.Columns.Add("EndTime");
-                dt.Columns.Add("EmployeeID");
-                dt.Columns.Add("EmployeeName");
-                dt.Columns.Add("Counter");
-                dt.Columns.Add("WorkDate");
-                dt.Columns.Add("Status");
-
-                foreach (var item in findnv)
-                {
-                    string workDate = isDateFilterUsed ? item.NgayLam.ToString("dd/MM/yyyy") : item.NgayLam.ToShortDateString(); // Định dạng ngày nếu sử dụng bộ lọc
-                    dt.Rows.Add(item.MaCaKip, item.GioBatDau, item.GioKetThuc, item.MaNhanVien, item.TenNhanVien, item.Quay, workDate, item.TrangThai);
-                }
-
-                dgv_LichSu.DataSource = dt;
-            }
-        }
-
-        private void txt_find_TextChanged(object sender, EventArgs e)
-        {
-            FilterData();
-        }
-
-        private void dtp_end_ValueChanged(object sender, EventArgs e)
-        {
-            FilterData();
-        }
-
-        private void dtp_start_ValueChanged(object sender, EventArgs e)
-        {
-            FilterData();
-        }
+        
     }
 }
